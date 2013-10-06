74b7f6444e52f8e662f0908336dc87c64797616f
vliegenthart <daniel.vliegenthart@gmail.com>
2013-10-06 22:32:14 +0200

    Another Spelling error

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,7 +12,7 @@
 require 'securerandom'
 
 def secure_token
-	token file = Rails.root.join('.secret')
+	token_file = Rails.root.join('.secret')
 	if File.exist?(token_file)
 		# Use the existing token.
 		File.read(token_file).chomp
