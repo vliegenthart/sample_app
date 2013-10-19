d8bd5d0ab5d881884ed7f8cf4cc883e0ee601561
vliegenthart <daniel.vliegenthart@gmail.com>
2013-10-19 17:07:57 +0200

    Finish enhancements exercises H9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,18 +12,26 @@ class UsersController < ApplicationController
   end
 
   def new
-  	@user = User.new
+    if signed_in?
+      redirect_to root_url
+    else
+  	 @user = User.new
+    end
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-      sign_in @user
-  		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
+    if signed_in?
+      redirect_to root_url
+    else
+    	@user = User.new(user_params)
+    	if @user.save
+        sign_in @user
+    		flash[:success] = "Welcome to the Sample App!"
+    		redirect_to @user
+    	else
+    		render 'new'
+    	end
+    end
   end
 
   def edit
@@ -39,9 +47,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:succes] = "User deleted."
-    redirect_to users_url
+    if(current_user != User.find(params[:id]))
+      User.find(params[:id]).destroy
+      flash[:succes] = "User deleted."
+    end
+      redirect_to users_url
   end
 
   private
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,10 +15,13 @@ describe "Authentication" do
 		before { visit signin_path }
 
  		describe "with invalid information" do
+ 			let(:user) { FactoryGirl.create(:user) }
  			before { click_button "Sign in" }
 
  			it { should have_title('Sign in') }
  			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+ 			it { should_not have_link('Profile',  href: user_path(user)) }
+ 			it { should_not have_link('Settings', href: edit_user_path(user)) }
 
  			describe "after visiting another page" do
  				before { click_link "Home" }
@@ -61,6 +64,17 @@ describe "Authentication" do
 
 	  				it "should render the desired protected page" do
 	  					expect(page).to have_title('Edit user')
+	  				end
+
+	  				describe "when signing in again" do
+	  					before do
+	  						delete signout_path
+	  						sign_in user
+	  					end
+
+	  					it "should render the default (profile) page" do
+	  						expect(page).to have_title(user.name)
+	  					end
 	  				end
 	  			end
 	  		end
